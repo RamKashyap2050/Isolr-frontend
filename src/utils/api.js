e2f031ab6c9e774c64b56f0b249110129d75a3cf
RamKashyap2050 <ramkashyap2050@gmail.com>
2026-01-25 18:53:53 -0500

    URL System uodated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-
+const  API_PROD_URL = import.meta.env.API_PROD_URL
 const api = axios.create({
     baseURL: API_BASE_URL,
     headers: {
