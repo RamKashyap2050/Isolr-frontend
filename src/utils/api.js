3470848db8af576f88b81c2c5c5564ccefed8b86
RamKashyap2050 <ramkashyap2050@gmail.com>
2026-01-25 22:55:46 -0500

    Added Envs for Prod

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-const  API_PROD_URL = import.meta.env.API_PROD_URL
 const api = axios.create({
-    baseURL: API_BASE_URL || API_PROD_URL,
+    // Use PROD URL if built for production, otherwise fallback to local URL
+    baseURL: import.meta.env.PROD 
+        ? import.meta.env.VITE_API_PROD_URL 
+        : (import.meta.env.VITE_API_URL || 'http://localhost:5001'),
     headers: {
         'Content-Type': 'application/json',
     },
